91100c71a05a96de1f0efd2efa26ddb67b1d39a7
Junicchi <24779257+KebabLord@users.noreply.github.com>
2019-08-28 23:48:07 +0300

    created urlhandler script & updated window labels

---
+++ scripts/linker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# URL HANDLER by Github/Kebablord
+
+case "$1" in
+	*mkv|*webm|*mp4|*youtube.com/watch*|*youtube.com/playlist*|*youtu.be*)
+		setsid mpv --loop -quiet "$1" >/dev/null 2>&1 & ;;
+	*png|*jpg|*jpe|*jpeg|*gif)
+		setsid feh "$1" >/dev/null 2>&1 & ;;
+#	*mp3|*flac|*opus|*mp3?source*)
+#		setsid mpv "$1" >/dev/null 2>&1 & ;;
+	*)
+		setsid firefox "$1" >/dev/null 2>&1 & ;;
+esac
